4afb5ddf53aa12109233b3a8f558a834aa8b484b
Yusadolat <yusadolat@gmail.com>
2017-03-31 17:38:27 +0000

    Modify user.componrnt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,33 @@ import { Component } from '@angular/core';
   <h1>Hello {{name}}</h1>
   <p><strong>Email:</strong> {{email}}</p>
   <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  <h3> Hobbies</h3>
+  {{hobbies}}
   `,
 })
 export class AppComponent  { 
-   name =  'Yusadolat';
-   email =  'Yusadolat@gmail.com',
-   address = {
-     street: '12 Yusuf St',
+   name: string;
+   email: string;
+   address: address;
+
+constructor(){
+  this.name =  'Yusadolat';
+   this.email =  'Yusadolat@gmail.com',
+   this.address = {
+     streets: '12 Yusuf St',
      city:  'Boston',
      state:  'MA'
    }
+   this.hobbies = ['Coding','sleeping','Sport'];
+}
+
  }
+
+
+ interface address {
+  street: string;
+  city: string;
+  state: string;
+ }
+
+
--- src/app/component/user.component.ts
+++ src/app/component/user.component.ts
@@ -3,17 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'user',
   template: `
-  <h1>Hello {{name}}</h1>
-  <p><strong>Email:</strong> {{email}}</p>
-  <p><strong>Address:</strong> {{address.street}} {{address.city}}, {{address.state}}</p>
+  <user></user>
   `,
 })
 export class UserComponent  { 
-   name =  'Yusadolat';
-   email =  'Yusadolat@gmail.com',
-   address = {
-     street: '12 Yusuf St',
-     city:  'Boston',
-     state:  'MA'
-   }
+   
  }
