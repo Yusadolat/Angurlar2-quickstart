ddd80f15423686fa42dbe7d9f924e25c8d15b215
Yusadolat <yusadolat@gmail.com>
2017-03-31 12:27:00 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>{{title}}</h1><h2> {{hero}} details</h2>`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  title = 'Tour of Heroes';
+  hero = 'Windstorms';
+
+ }
